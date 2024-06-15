b93e4da1677535d2b6f008ab0b624554c685a002
Isaac Vazquez <ivzc07@gmail.com>
2024-06-14 21:29:58 -0600

    ADD hidden block mobile

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,7 +1,7 @@
 import UserInfo from "./UserInfo"
 export default function SideBar (){
     return(
-        <aside className="col-start-1 col-end-2 row-start-2 row-end-3 bg-[#1f2024]" >
+        <aside className="col-start-1 col-end-2 row-start-2 row-end-3 bg-[#1f2024] hidden md:block" >
             <div className="h-[42px] w-[342] inter-font text-1xl text-[#f6f3fc] p-4 mt-1 flex justify-between items-center bg-[#1f2024]">
                 <p>CANALES RECOMENDADOS</p>
             </div>
